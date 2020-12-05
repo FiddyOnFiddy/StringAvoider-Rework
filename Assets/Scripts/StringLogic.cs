0920dfb91c2fe3c23a8bc0e03c49981498ea7f5f
FiddyOnFiddy <matthewbiggins@hotmail.co.uk>
2020-12-05 10:05:10 +0000

    Spawned String Points

--- Assets/Scripts/StringLogic.cs
+++ Assets/Scripts/StringLogic.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class StringLogic : MonoBehaviour
 {
+    [SerializeField] int stringResolution;
+    [SerializeField] float segmentLength = 0.025f;
+    [SerializeField] float radius = 0.1f;
+
+    [SerializeField] Vector2 mousePosition, previousMousePosition, mouseDelta;
+    [SerializeField] Vector2 headPosition;
+
+    float radians;
+
+    [SerializeField] GameObject stringPointPrefab;
+    [SerializeField] List<GameObject> stringPointsList;
+
+    [SerializeField] Transform spawnPoint;
+    
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        stringPointsList = new List<GameObject>();
+
+        for (int i = 0; i < stringResolution; i++)
+        {
+            radians = 12 * Mathf.PI * i / stringResolution + Mathf.PI / 4;
+
+            Vector2 stringPointPosition = new Vector2((spawnPoint.position.x + radius * Mathf.Cos(radians)), spawnPoint.position.y + radius * Mathf.Sin(radians));
+
+            stringPointsList.Add(Instantiate(stringPointPrefab, stringPointPosition, Quaternion.identity, this.transform));
+    }
     }
 
     // Update is called once per frame
